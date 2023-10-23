12c67355ac18b52e42af4bf60430708b1cbb3780
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-10-23 20:49:08 +0400

    task

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using _2410ok.models;
+
+namespace _2410ok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //        List<Person> People = new List<Person>
+            //    {
+            //        new Person { Name = "Yusif", Surname = "Celilli", Age = 19 },
+            //        new Person { Name = "Zeynal", Surname = "Celilov", Age = 21 },
+            //        new Person { Name = "Aygun", Surname = "Kazimova", Age = 51 },
+            //        new Person { Name = "Namiq", Surname = "Qaracuxurlu", Age = 53 },
+            //    };
+
+
+            //        var yusifPersons = People.FindAll(person => person.Name == "Yusif");
+            //        Console.WriteLine("Adı Yusif olan kişiler:");
+            //        PrintPeople(yusifPersons);
+
+
+            //        var ovaEndingSurnames = People.FindAll(person => person.Surname.EndsWith("ov") || person.Surname.EndsWith("ova"));
+            //        Console.WriteLine("Soyadı 'ov' və 'ova' ilə bitən kişiler:");
+            //        PrintPeople(ovaEndingSurnames);
+
+
+            //        var age20Plus = People.FindAll(person => person.Age >= 20);
+            //        Console.WriteLine("Yaşı 20+ olan kişiler:");
+            //        PrintPeople(age20Plus);
+
+            //    }
+
+            //    static void PrintPeople(List<Person> people)
+            //    {
+            //        foreach (var person in people)
+            //        {
+            //            Console.WriteLine($"Name: {person.Name}, Surname: {person.Surname}, Age: {person.Age}");
+            //        }
+
+            //task2
+            //List<Exam> exams = new List<Exam>();
+
+            //// Birkaç imtahan yaradın və siyahıya əlavə edin
+            //exams.Add(new Exam
+            //{
+            //    Subject = "Math",
+            //    ExamDuration = TimeSpan.FromHours(2),
+            //    StartDate = DateTime.Now - TimeSpan.FromDays(5),
+            //    EndDate = DateTime.Now - TimeSpan.FromDays(5) + TimeSpan.FromHours(2)
+            //});
+
+            //exams.Add(new Exam
+            //{
+            //    Subject = "Science",
+            //    ExamDuration = TimeSpan.FromHours(3),
+            //    StartDate = DateTime.Now - TimeSpan.FromDays(3),
+            //    EndDate = DateTime.Now - TimeSpan.FromDays(3) + TimeSpan.FromHours(2)
+            //});
+
+            //exams.Add(new Exam
+            //{
+            //    Subject = "History",
+            //    ExamDuration = TimeSpan.FromHours(1),
+            //    StartDate = DateTime.Now - TimeSpan.FromHours(1),
+            //    EndDate = DateTime.Now - TimeSpan.FromMinutes(30)
+            //});
+            //ardini yaza bilmedim 
+
+
+        }
+    }
+    }
---
+++ models/person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2410ok.models
+{
+        class Person
+        {
+        //task1
+        //    public string Name { get; set; }
+        //    public string Surname { get; set; }
+        //    public int Age { get; set; }
+        }
+    }
+
